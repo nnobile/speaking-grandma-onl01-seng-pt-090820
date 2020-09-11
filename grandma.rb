6c82b4605eb8d1b8b7f296ac18afebdc556a534e
Nick Nobile <nickcnobile@gmail.com>
2020-09-11 02:55:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,6 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
     elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.upcase
+  else phrase == phrase.upcase
    return "NO, NOT SINCE 1938!"
 end
